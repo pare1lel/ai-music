66eb5e78402f476ed55b45536ea6a4e782950ee1
Doverhi <2668940140@qq.com>
2023-12-22 19:43:12 +0800

    convert main to a model

--- main.py
+++ main.py
@@ -48,32 +48,33 @@ def mutate(melody):
             melody[i], melody[j] = melody[j], melody[i]
     return melody,
 
-# 创建适应度类和个体类
-creator.create("FitnessMax", base.Fitness, weights=(1.0,))
-creator.create("Individual", list, fitness=creator.FitnessMax)
+if __name__ == '__main__':
+    # 创建适应度类和个体类
+    creator.create("FitnessMax", base.Fitness, weights=(1.0,))
+    creator.create("Individual", list, fitness=creator.FitnessMax)
 
-# 注册遗传算子
-toolbox = base.Toolbox()
-toolbox.register("note", random.randint, MELODY_MIN, MELODY_MAX)
-toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.note, n=MELODY_LENGTH)
-toolbox.register("population", tools.initRepeat, list, toolbox.individual)
-toolbox.register("evaluate", evaluate)
-toolbox.register("mate", tools.cxTwoPoint)
-toolbox.register("mutate", mutate)
-toolbox.register("select", tools.selBest)
+    # 注册遗传算子
+    toolbox = base.Toolbox()
+    toolbox.register("note", random.randint, MELODY_MIN, MELODY_MAX)
+    toolbox.register("individual", tools.initRepeat, creator.Individual, toolbox.note, n=MELODY_LENGTH)
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+    toolbox.register("evaluate", evaluate)
+    toolbox.register("mate", tools.cxTwoPoint)
+    toolbox.register("mutate", mutate)
+    toolbox.register("select", tools.selBest)
 
-# 设置初始种群
-population = toolbox.population(n=GEN_SIZE)
+    # 设置初始种群
+    population = toolbox.population(n=GEN_SIZE)
 
-# 运行遗传算法
-for gen in range(GEN_NUM):
-    offspring = algorithms.varAnd(population, toolbox, cxpb=0.5, mutpb=0.15)
-    fits = toolbox.map(toolbox.evaluate, offspring)
-    for fit, ind in zip(fits, offspring):
-        ind.fitness.values = fit
-    population = toolbox.select(population + offspring, k=GEN_SIZE)
+    # 运行遗传算法
+    for gen in range(GEN_NUM):
+        offspring = algorithms.varAnd(population, toolbox, cxpb=0.5, mutpb=0.15)
+        fits = toolbox.map(toolbox.evaluate, offspring)
+        for fit, ind in zip(fits, offspring):
+            ind.fitness.values = fit
+        population = toolbox.select(population + offspring, k=GEN_SIZE)
 
-# 输出最优旋律
-best = tools.selBest(population, k=1)[0]
-print("Best melody:", best)
-print("Best value:", evaluate(best)[0])
+    # 输出最优旋律
+    best = tools.selBest(population, k=1)[0]
+    print("Best melody:", best)
+    print("Best value:", evaluate(best)[0])
